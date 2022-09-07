449a6129f1ccaf7ec7c3389de38ac5fef6137d39
Muhammad Rafly Sadewa <raflysdw25@gmail.com>
2022-09-07 09:48:24 +0700

    finish migration Camp, Camp Benefits, Checkout

---
+++ database/migrations/2022_09_07_015936_create_camp_benefits_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('camp_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+
+            // Foreign Key : 1st Method (camp_id dalam bentuk unsigned)
+            $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+
+            // Foreign Key : 2nd Method (Syarat: nama kolom harus sesuai dengan nama table relasi)
+            // $table->foreignId('camp_id')->constrained()
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
